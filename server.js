b2b9912d99f7701ea39d25a4dde9b337aa410ca2
BackinR2D2 <cristimlg334@gmail.com>
2020-07-17 16:52:11 +0300

    todolist

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 5000
+const path = require('path');
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.set('views', path.join(__dirname, 'views'))
+
+const list = [];
+
+app.get('/', (req, res) => {
+    res.render('home', { list: list })
+})
+
+app.post('/', (req, res) => {
+    list.push(req.body.todo)
+    res.redirect('/')
+})
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
+})
