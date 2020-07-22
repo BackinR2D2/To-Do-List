6301efcd6904df45eb54d0a142e183d3143d8377
BackinR2D2 <cristimlg334@gmail.com>
2020-07-22 13:18:31 +0300

    refactor

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 5000
 const path = require('path');
 
 app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.set('views', path.join(__dirname, 'views'))
